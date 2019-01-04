c7333dbe96f57d785bf6052ec3ceb755810dd14b
Callum Stow <43756591+Stow710@users.noreply.github.com>
2019-01-04 17:18:33 +0000

    Add files via upload

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,14 @@
+playNo1 = function() {
+	var videoPanel = document.getElementById('vid');
+	videoPanel.innerHTML = '<iframe width="1120" height="630" src="https://www.youtube.com/embed/UnyLfqpyi94" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+}
+
+playNo2 = function() { 
+	var videoPanel = document.getElementById('vid');
+	videoPanel.innerHTML = '<iframe width="1120" height="630" src="https://www.youtube.com/embed/txCCYBMKdB0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+}
+
+playNo3 = function() {
+	var videoPanel = document.getElementById('vid');
+	videoPanel.innerHTML = '<iframe width="1120" height="630" src="https://www.youtube.com/embed/DC_TrsY7U3A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+}
