75f302f3d595f22165ee6d9d1c5926c9db357204
Callum Stow <43756591+Stow710@users.noreply.github.com>
2019-01-09 22:47:13 +0000

    Update JavaScript.js

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -12,39 +12,3 @@ playNo3 = function() {
 	var videoPanel = document.getElementById('vid');
 	videoPanel.innerHTML = '<iframe width="1120" height="630" src="https://www.youtube.com/embed/DC_TrsY7U3A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 }
-
-$('.fav-button').on('click', function() {
-   $(this).toggleClass('is-active');
-});
-
-$('.Minus_Btn').on('click', function(e) {
-    e.preventDefault();
-    var $this = $(this);
-    var $input = $this.closest('div').find('input');
-    var value = parseInt($input.val());
- 
-    if (value &amp;gt; 1) {
-        value = value - 1;
-    } else {
-        value = 0;
-    }
- 
-  $input.val(value);
- 
-});
- 
-$('.Plus_Btn').on('click', function(e) {
-    e.preventDefault();
-    var $this = $(this);
-    var $input = $this.closest('div').find('input');
-    var value = parseInt($input.val());
- 
-    if (value &amp;lt; 100) {
-        value = value + 1;
-    } else {
-        value =100;
-    }
- 
-    $input.val(value);
-});
-
