5605749e3b3496854dbc65b88e5e95fced74d2e5
Callum Stow <43756591+Stow710@users.noreply.github.com>
2019-01-10 12:18:35 +0000

    Add files via upload

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,3 +1,4 @@
+
 playNo1 = function() {
 	var videoPanel = document.getElementById('vid');
 	videoPanel.innerHTML = '<iframe width="1120" height="630" src="https://www.youtube.com/embed/UnyLfqpyi94" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
@@ -12,3 +13,4 @@ playNo3 = function() {
 	var videoPanel = document.getElementById('vid');
 	videoPanel.innerHTML = '<iframe width="1120" height="630" src="https://www.youtube.com/embed/DC_TrsY7U3A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 }
+
